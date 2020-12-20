ebcebe61c5eb2146eb7ef3c2a0f3e7a11638acfa
Kamal Hossain <hirethedeveloperkamal@gmail.com>
2020-12-20 12:09:21 +0600

    working in local environment perfectly

---
+++ services/api/server.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+
+// resolving  bug!! 
+app.use(cors())
+
+// Server port
+const port = process.env.PORT || 80
+
+// Database configs
+const DB = 'mongodb://mongo/myproject?retryWrites=true&w=majority' // TODO for cloud
+// const DB = 'mongodb://localhost:27017/myproject?retryWrites=true&w=majority'
+const dbName = 'myproject'
+let db
+let dbCollection
+
+// Connect to mongo
+MongoClient.connect(
+  DB,
+  {
+    useUnifiedTopology: true,
+  },
+  function (err, client) {
+    if (err) {
+      // throw err
+      console.log('mongodb conneciton failed')
+      // return
+    }
+    db = client.db(dbName)
+    dbCollection = db.collection('counterCollection')
+    console.log('MongoDB connected at > ' + DB)
+  }
+)
+
+app.get('/', function (req, res) {
+  // Get counter value from mongodb
+  dbCollection
+    .find()
+    .toArray()
+    .then((results) => {
+      if (results.length === 0) {
+        console.log('inserting new document')
+        dbCollection.insertOne({ count: 0 })
+        res.status(200).json(0)
+      }
+      console.log(results[0].count)
+      res.status(200).json(results[0].count)
+    })
+})
+
+app.get('/update', function (req, res) {
+  //update counter value from mongodb
+  dbCollection
+    .find()
+    .toArray()
+    .then((results) => {
+      if (results.length === 0) {
+        console.log('inserting new document')
+        dbCollection.insertOne({ count: 0 })
+        res.status(200).json(0)
+      }
+      console.log(results.length)
+      // updating counter value
+      dbCollection
+        .findOneAndUpdate(
+          { _id: results[0]._id },
+          { $set: { count: results[0].count + 1 } },
+          { new: true }
+        )
+        .then((result) => {
+          console.log(result)
+          res.status(200).json(result.value.count)
+        })
+    })
+})
+
+app.listen(port, function () {
+  console.log(`listening on ${port}`)
+})
---
+++ services/client/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = React.useState(0)
+
+  React.useEffect(() => {
+    fetch('http://localhost:80')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          setCount(result)
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+  }, [])
+
+  const updateDB = () => {
+    fetch('http://localhost:80/update')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          setCount(result)
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+  }
+
+  return (
+    <div className="App">
+      <h1>This is a react app</h1>
+      {count && (
+        <>
+          <h1>Count value from mongoDB = {count}</h1>
+          <button onClick={updateDB}>Update db</button>
+        </>
+      )}
+    </div>
+  )
+}
+export default App
